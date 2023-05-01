ba6940454ee3e0f4797a7e56622ad9998d495111
Nikita <droider.ns@gmail.com>
2023-05-01 13:51:18 +0300

    Add String_repeat.py

---
+++ 8_kyu/String_repeat.py
@@ -0,0 +1,12 @@
+# Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+# Examples (input -> output)
+#
+# 6, "I"     -> "IIIIII"
+# 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+def repeat_str(repeat, string):
+    return ''.join(string * repeat)
+
+
+if __name__ == "__main__":
+    print(repeat_str(3, 'hello'))
