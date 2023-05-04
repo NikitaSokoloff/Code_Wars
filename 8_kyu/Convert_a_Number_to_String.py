557e1c051b5aebab72dcfd6c36a8974b06ad3c1e
Nikita <droider.ns@gmail.com>
2023-05-04 11:46:13 +0300

    Add Convert_a_Number_to_String.py

---
+++ 8_kyu/Convert_a_Number_to_String.py
@@ -0,0 +1,21 @@
+# We need a function that can transform a number (integer) into a string.
+#
+# What ways of achieving this do you know?
+# Examples (input --> output):
+#
+# 123  --> "123"
+# 999  --> "999"
+# -100 --> "-100"
+#
+
+
+def number_to_string(num):
+    return str(num)
+
+
+if __name__ == "__main__":
+    number_to_string(67)
+    number_to_string(79585)
+    number_to_string(79585)
+    number_to_string(1 + 2)
+    number_to_string(1 - 2)
